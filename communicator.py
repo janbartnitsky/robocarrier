9e748dcd248bf57d53bece9f8cc9331d67931e7d
Jan Bartnitsky <baja@gurtam.com>
2021-09-17 18:46:26 +0300

    communicator now passes test with result 0

--- communicator.py
+++ communicator.py
@@ -24,7 +24,8 @@ for i in range(0,map_name):
 	j = citymap[i].find('.')
 	if j != -1:
 		break
-sys.stdout.write(str(i) + ' ' + str(j) + '\n')
+sys.stdout.write(str(i+1) + ' ' + str(j+1) + '\n')
+sys.stdout.flush()
 
 # perform iterations
 for _ in range(0, iterations_count):
@@ -34,3 +35,4 @@ for _ in range(0, iterations_count):
 		input_line = sys.stdin.readline()
 		start_x, start_y, finish_x, finish_y = map(int, input_line.split())
 	sys.stdout.write('SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n')
+	sys.stdout.flush()
